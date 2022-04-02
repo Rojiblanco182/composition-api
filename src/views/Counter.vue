9cfee1957e27641cf32d0371227478d780972534
Rojiblanco182 <salinas.182@hotmail.com>
2022-04-02 16:59:06 +0200

    started repo

---
+++ src/views/Counter.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>Counter</h1>
+  <h2>{{ counter }}</h2>
+
+  <button @click="increaseCounter">+1</button>
+  <button @click="decreaseCounter">-1</button>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  name: 'Counter',
+  setup() {
+    const counter = ref(3)
+
+    return { 
+      counter,
+      increaseCounter: () => counter.value++,
+      decreaseCounter: () => counter.value--
+      }
+  }
+}
+</script>
+
+<style>
+
+</style>
