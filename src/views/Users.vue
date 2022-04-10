d6f21ad928a38399c9d5938a27366c0f6a927ab8
Rojiblanco182 <salinas.182@hotmail.com>
2022-04-10 21:45:07 +0200

    created new composable

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -18,48 +18,27 @@
 </template>
 
 <script>
-import axios from 'axios'
-import { ref } from 'vue'
+
+import useUsers from '@/composables/useUsers';
 
 export default {
   setup() {
-    const users = ref([])
-    const isLoading = ref(true)
-    const currentPage = ref(1)
-    const errorMessage = ref()
-
-    const getUsers = async(page = 1) => {
-      if (page <= 0) {
-        page = 1
-      }
-
-      isLoading.value = true
-
-      const { data } = await axios.get('https://reqres.in/api/users', {
-        params: { page }
-      })
-
-      if (data.data.length > 0) {
-        users.value = data.data
-        currentPage.value = page
-        errorMessage.value = null
-      } else if (currentPage.value > 0) {
-        errorMessage.value = 'No hay más usuarios'
-      }
-
-      isLoading.value = false
-    }
-
-    getUsers()
+    const {
+      users,
+      isLoading,
+      currentPage,
+      errorMessage,
+      nextPage,
+      previousPage
+    } = useUsers()
 
     return {
       users,
       isLoading,
       currentPage,
       errorMessage,
-
-      nextPage: () => getUsers(currentPage.value + 1),
-      previousPage: () => getUsers(currentPage.value - 1)
+      nextPage,
+      previousPage
     }
   }
 }
